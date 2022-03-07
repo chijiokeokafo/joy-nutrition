7bb0e8dc3c40786af45e86b20472e543b73bd98c
chijiokeokafo <chijiokeokafo@gmail.com>
2022-03-06 21:33:53 -0500

    Updating favicon and route titles

--- src/router/index.js
+++ src/router/index.js
@@ -5,33 +5,122 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: {
+      title: 'JOY Nutrition - HOME',
+      metaTags: [
+        {
+          name: 'description',
+          content: 'JOY Nutrition - HOME'
+        }
+      ]
+    }
   },
   {
     path: '/about',
     name: 'About',
-    component: () => import('../views/About.vue')
+    component: () => import('../views/About.vue'),
+    meta: {
+      title: 'JOY Nutrition - ABOUT',
+      metaTags: [
+        {
+          name: 'description',
+          content: 'JOY Nutrition - ABOUT'
+        }
+      ]
+    }
   },
   {
     path: '/services',
     name: 'Services',
-    component: () => import('../views/Services.vue')
+    component: () => import('../views/Services.vue'),
+    meta: {
+      title: 'JOY Nutrition - SERVICES',
+      metaTags: [
+        {
+          name: 'description',
+          content: 'JOY Nutrition - SERVICES'
+        }
+      ]
+    }
   },
   {
     path: '/resources',
     name: 'Resources',
-    component: () => import('../views/Resources.vue')
+    component: () => import('../views/Resources.vue'),
+    meta: {
+      title: 'JOY Nutrition - RESOURCES',
+      metaTags: [
+        {
+          name: 'description',
+          content: 'JOY Nutrition - RESOURCES'
+        }
+      ]
+    }
   },
   {
     path: '/contact',
     name: 'Contact',
-    component: () => import('../views/Contact.vue')
+    component: () => import('../views/Contact.vue'),
+    meta: {
+      title: 'JOY Nutrition - CONTACT',
+      metaTags: [
+        {
+          name: 'description',
+          content: 'JOY Nutrition - CONTACT'
+        }
+      ]
+    }
   }
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+// This callback runs before every route change, including on page load.
+router.beforeEach((to, from, next) => {
+  // This goes through the matched routes from last to first, finding the closest route with a title.
+  // e.g., if we have `/some/deep/nested/route` and `/some`, `/deep`, and `/nested` have titles,
+  // `/nested`'s will be chosen.
+  const nearestWithTitle = to.matched.slice().reverse().find(r => r.meta && r.meta.title)
+
+  // Find the nearest route element with meta tags.
+  const nearestWithMeta = to.matched.slice().reverse().find(r => r.meta && r.meta.metaTags)
+
+  const previousNearestWithMeta = from.matched.slice().reverse().find(r => r.meta && r.meta.metaTags)
+
+  // If a route with a title was found, set the document (page) title to that value.
+  if (nearestWithTitle) {
+    document.title = nearestWithTitle.meta.title
+  } else if (previousNearestWithMeta) {
+    document.title = previousNearestWithMeta.meta.title
+  }
+
+  // Remove any stale meta tags from the document using the key attribute we set below.
+  Array.from(document.querySelectorAll('[data-vue-router-controlled]')).map(el => el.parentNode.removeChild(el))
+
+  // Skip rendering meta tags if there are none.
+  if (!nearestWithMeta) return next()
+
+  // Turn the meta tag definitions into actual elements in the head.
+  nearestWithMeta.meta.metaTags.map(tagDef => {
+    const tag = document.createElement('meta')
+
+    Object.keys(tagDef).forEach(key => {
+      tag.setAttribute(key, tagDef[key])
+    })
+
+    // We use this to track which meta tags we create so we don't interfere with other ones.
+    tag.setAttribute('data-vue-router-controlled', '')
+
+    return tag
+  })
+  // Add the meta tags to the document head.
+    .forEach(tag => document.head.appendChild(tag))
+
+  next()
 })
 
 export default router
